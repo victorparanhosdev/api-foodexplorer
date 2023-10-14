cde0b9d4890c912f912e07f39798d543e407659e
Victor Paranhos <victor_paranhos@hotmail.com>
2023-10-14 13:44:40 -0300

    .

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT ||4000;
 app.listen(PORT, () =>
   console.log(`Servidor Iniciado em http://localhost:${PORT}`)
 );
